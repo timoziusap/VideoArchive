0af7606ee0caea2a89ff4a1b668d1b46ab32209b
Pascal Witt <witt.pascal@outlook.de>
2026-06-09 10:09:03 +0200

    "Test Push"

--- src/main/java/hs/aalen/VideoArchiveApplication.java
+++ src/main/java/hs/aalen/VideoArchiveApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//Zugriffs und Push test
 public class VideoArchiveApplication{
 
 	public static void main(String[] args) {
