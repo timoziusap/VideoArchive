f915d59a203ce30e00c4ab4414e3d9705156f0a7
Niclas Eberlein <eberleinniclas@gmail.com>
2026-06-16 11:19:06 +0200

    hello world

--- src/main/java/hs/aalen/VideoArchiveApplication.java
+++ src/main/java/hs/aalen/VideoArchiveApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 //Zugriffs und Push test
 //Timo test
 //Niclas Test
+//hello world
 public class VideoArchiveApplication{
 
 	public static void main(String[] args) {
