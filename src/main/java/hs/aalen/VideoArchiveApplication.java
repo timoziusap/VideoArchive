f226007ccb7ed62c2950284140fab1367f28c921
Timo Appt <3009368@studmail.htw-aalen.de>
2026-06-16 09:43:41 +0200

    Ci workflow

--- src/main/java/hs/aalen/VideoArchiveApplication.java
+++ src/main/java/hs/aalen/VideoArchiveApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 //Zugriffs und Push test
-//Timo test
+//Timo test 
 public class VideoArchiveApplication{
 
 	public static void main(String[] args) {
