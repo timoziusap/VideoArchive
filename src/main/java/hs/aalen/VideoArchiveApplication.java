04588dd3b4cce7e6e65f17aa1a849c68580da361
Timo Appt <3009368@studmail.htw-aalen.de>
2026-06-16 10:15:38 +0200

    Test ob ci geht

--- src/main/java/hs/aalen/VideoArchiveApplication.java
+++ src/main/java/hs/aalen/VideoArchiveApplication.java
@@ -4,8 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//Zugriffs und Push test
-//Timo test 
+
+//CI Test
+
 public class VideoArchiveApplication{
 
 	public static void main(String[] args) {
